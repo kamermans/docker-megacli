86586530c10371268f68942805f753b2932e8bcb
Steve Kamerman <stevekamerman@gmail.com>
2016-09-07 16:12:53 -0400

    Typo in man command for storcli help

--- resources/megacli_profile.sh
+++ resources/megacli_profile.sh
@@ -3,7 +3,7 @@ man() {
     shopt -s nocasematch
     if [[ $@ =~ "megacli" ]]; then
         command megacli -help | less -S
-    elif [[ $@ =~ "megacli" ]]; then
+    elif [[ $@ =~ "storcli" ]]; then
         command storcli -help | less -S
     else
         command man "$@"
